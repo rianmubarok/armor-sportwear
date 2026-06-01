db38c4ff81b86dbaf4be04dc687eb905ac17ab06
Rian <fitriyan606@gmail.com>
2026-06-01 11:41:01 +0700

    Ubah link Pesan Sekarang di hero mengarah ke #kontak

--- resources/views/components/frontend/hero.blade.php
+++ resources/views/components/frontend/hero.blade.php
@@ -22,7 +22,7 @@
                         Lihat Katalog
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
                     </a>
-                    <a href="{{ url('/request-order') }}" class="btn-outline">
+                    <a href="#kontak" class="btn-outline">
                         Pesan Sekarang
                     </a>
                 </div>
